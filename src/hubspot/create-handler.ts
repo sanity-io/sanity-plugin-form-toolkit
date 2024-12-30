40da0d63aabe554f4d85651884886ac1c9baea29
ChrisLaRocque <larocque.christopher@gmail.com>
2024-12-29 23:33:40 -0500

    Docs + rename export for consistency

--- src/hubspot/create-handler.ts
+++ src/hubspot/create-handler.ts
@@ -1,6 +1,6 @@
 import createHandler from '../shared/create-handler'
 import {fetchHubSpotData} from './fetch-hubspot-data'
 
-export const hubspotHandler = ({token}: {token: string}) => {
+export const hubSpotHandler = ({token}: {token: string}) => {
   return createHandler(() => fetchHubSpotData({token}))
 }
--- src/index.ts
+++ src/index.ts
@@ -1,34 +1,16 @@
-// import {definePlugin} from 'sanity'
-
-// interface MyPluginConfig {
-//   /* nothing here yet */
-// }
-
-// /**
-//  * Usage in `sanity.config.ts` (or .js)
-//  *
-//  * ```ts
-//  * import {defineConfig} from 'sanity'
-//  * import {myPlugin} from 'sanity-plugin-form-toolkit'
-//  *
-//  * export default defineConfig({
-//  *   // ...
-//  *   plugins: [myPlugin()],
-//  * })
-//  * ```
-//  */
-// export const myPlugin = definePlugin<MyPluginConfig | void>((config = {}) => {
-//   // eslint-disable-next-line no-console
-//   console.log('hello from sanity-plugin-form-toolkit')
-//   return {
-//     name: 'sanity-plugin-form-toolkit',
-//   }
-// })
 // import {formSchema} from './form-schema'
 // import {formiumInput} from './formium'
 import {hubSpotInput} from './hubspot'
-import {hubspotHandler} from './hubspot/create-handler'
+import {hubSpotHandler} from './hubspot/create-handler'
+import {fetchHubSpotData} from './hubspot/fetch-hubspot-data'
 import {mailchimpInput} from './mailchimp'
 import {fetchMailchimpData, mailchimpHandler} from './mailchimp/create-handler'
 
-export {fetchMailchimpData, hubspotHandler, hubSpotInput, mailchimpHandler, mailchimpInput}
+export {
+  fetchHubSpotData,
+  fetchMailchimpData,
+  hubSpotHandler,
+  hubSpotInput,
+  mailchimpHandler,
+  mailchimpInput,
+}
