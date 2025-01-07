30b00eb7a590ddf7712f3b9741e18feb855f2f4b
ChrisLaRocque <larocque.christopher@gmail.com>
2025-01-06 22:07:40 -0500

    Update section-input.tsx
    I mean just look at this

--- src/form-schema/schema-types/components/section-input.tsx
+++ src/form-schema/schema-types/components/section-input.tsx
@@ -1,18 +1,35 @@
-import {useMemo} from 'react'
+import {useEffect, useMemo, useState} from 'react'
 import {useObservable} from 'react-rx'
 import {type ObjectInputProps, useDocumentStore, useFormValue} from 'sanity'
 
 export const SectionInput = (props: ObjectInputProps) => {
+  console.log('props', props)
   const id = useFormValue(['_id'])
   const documentStore = useDocumentStore()
   const observable = useMemo(() => {
     // @ts-expect-error idk
     return documentStore.listenQuery(`*[_type == 'form' && _id == $id][0]`, {id}, {})
   }, [documentStore, id])
-  const results = useObservable(observable, [])
+  const results = useObservable(observable, {})
+  const list = () => {
+    // console.log('list called')
+    // console.log('results', results)
+    const fields = []
+    if (results && results.sections) {
+      for (const section of results.sections) {
+        if (section.fields) {
+          for (const field of section.fields) {
+            if (field.name) {
+              fields.push({value: field.name, title: field.label ?? field.name})
+            }
+          }
+        }
+      }
+    }
+    console.log('fields', fields)
+    return fields
+  }
 
-  // console.log('props', props)
-  // console.log('results', results)
   const newProps = {
     ...props,
     schemaType: {
@@ -25,13 +42,14 @@ export const SectionInput = (props: ObjectInputProps) => {
             ...field.type,
             fields: field.type.fields.map((field) => {
               if (field.name !== 'field') return field
+              console.log('field.name is field', field)
               return {
                 ...field,
                 type: {
                   ...field.type,
                   options: {
                     ...field.type.options,
-                    list: results,
+                    list: list(),
                   },
                 },
               }
@@ -41,5 +59,6 @@ export const SectionInput = (props: ObjectInputProps) => {
       }),
     },
   }
+  console.log('newprops', newProps)
   return newProps.renderDefault(newProps)
 }
