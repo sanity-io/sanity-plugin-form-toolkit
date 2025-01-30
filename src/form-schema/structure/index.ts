49f7d12cdea3c2606df1960fce458c89809f6d24
ChrisLaRocque <larocque.christopher@gmail.com>
2025-01-29 19:00:50 -0500

    fix: linting errors

--- src/form-schema/structure/index.ts
+++ src/form-schema/structure/index.ts
@@ -3,7 +3,6 @@ import type {DefaultDocumentNodeResolver} from 'sanity/structure'
 import {DocumentView} from './document-view'
 
 export const defaultDocumentNode: DefaultDocumentNodeResolver = (S, {schemaType}) => {
-  console.log('schemaType', schemaType)
   // Conditionally return a different configuration based on the schema type
   if (schemaType === 'form') {
     return S.document().views([S.view.form(), S.view.component(DocumentView).title('Web')])
